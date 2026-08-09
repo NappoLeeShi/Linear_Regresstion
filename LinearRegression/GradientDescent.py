c9545d53894289dc2ab1774502b8c2b22bbfaa12
NappoLeeShi <Khuattrongthuan@gmail.com>
2026-08-09 21:29:00 +0700

    add gd

---
+++ LinearRegression/GradientDescent.py
@@ -0,0 +1,22 @@
+class LinearRegressionGD:
+    def __init__(self, lr=0.01, epochs=1000):
+        self.lr = lr
+        self.epochs = epochs
+        self.w = None
+        self.b = None
+
+    def fit(self, X, y):
+        m, n = X.shape
+        self.w = np.zeros(n)
+        self.b = 0.0
+
+        for epoch in range(self.epochs):
+            y_pred = np.dot(X, self.w) + self.b
+            dw = (1 / m) * np.dot(X.T, (y_pred - y))
+            db = (1 / m) * np.sum(y_pred - y)
+
+            self.w -= self.lr * dw
+            self.b -= self.lr * db
+
+    def predict(self, X):
+        return np.dot(X, self.w) + self.b
